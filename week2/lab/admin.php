4d713f516f649edb0470a186bca35f15d7305f34
001305466 <001305466@N208C3.labs.neit.edu>
2015-10-19 22:40:09 -0400

    add week 3

--- week2/lab/admin.php
+++ week2/lab/admin.php
@@ -1,9 +1,23 @@
-<?php require_once './autoload.php'; ?>
+<?php
+require_once './autoload.php';
+
+$logout = filter_input(INPUT_GET, 'logout');
+
+if ($logout == 1) {
+    $_SESSION['user_id'] = null;
+}
+
+if (!isset($_SESSION['user_id'])) {
+    header('Location: index.php');
+} else if (isset($_SESSION['user_id'])) {
+    echo '<a href="?logout=1">Logout</a>';
+}
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Sign Up</title>
+        <title>Admin</title>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <!-- Optional theme -->
@@ -13,7 +27,9 @@
     <body>
         <p>You are on the admin page<p>
 
-            <input type="submit" value="Log out" id="logout" class="btn btn-primary" />
+<?php ?>
+
+            <!--<input type="submit" value="Log out" id="logout" class="btn btn-primary" />-->
     </body>
 </html>
 
